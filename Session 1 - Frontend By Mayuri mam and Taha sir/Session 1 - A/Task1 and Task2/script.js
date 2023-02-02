36b2e597a77bbe6e03a8a1bb5c13534d178b35b9
FAIZAN ALI IQBAL ALI <67515374+Faizan2907@users.noreply.github.com>
2023-02-03 05:02:51 +0530

    Add files via upload

---
+++ Session 1 - Frontend By Mayuri mam and Taha sir/Session 1 - A/Task1 and Task2/script.js
@@ -0,0 +1,31 @@
+
+
+////////////////////////////////
+// Array Functions Example
+
+const array = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// console.log(array);
+
+// array.push('Saturday');
+// console.log(array);
+
+// const poppedValue = array.pop();
+// console.log(poppedValue, array);
+
+// array.splice(0,1);
+// console.log(array);
+
+// const index = array.indexOf('Tuesday');
+// console.log(index);
+
+// function checkDay(day){
+//     return day === 'Friday';
+// }
+// const index = array.findIndex(checkDay);
+
+// const index = array.findIndex(day => day === 'Friday');
+// console.log(index);
+
+// const copyArray = array.filter(day => day !== 'Friday' && day !== 'Saturday');
+// console.log(copyArray);
