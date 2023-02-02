a40a733c93f4e1d7c62802f53b1e0d3db7915f4d
FAIZAN ALI IQBAL ALI <67515374+Faizan2907@users.noreply.github.com>
2023-02-03 04:06:16 +0530

    Add files via upload

---
+++ Session2 - A - By Taha Sir/Session2 - A - By Taha Sir/Reactive Form/hero.ts
@@ -0,0 +1,11 @@
+export class Hero {
+
+    constructor(
+      public id: number,
+      public name: string,
+      public hobbies: string,
+      public gender: string,
+      public email?: string
+    ) {  }
+  
+  }
