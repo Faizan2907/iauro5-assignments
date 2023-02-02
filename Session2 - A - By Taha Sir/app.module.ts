a1357bddb29875acc6d1e2ddb666bc2158699122
FAIZAN ALI IQBAL ALI <67515374+Faizan2907@users.noreply.github.com>
2023-02-03 04:04:42 +0530

    Add files via upload

---
+++ Session2 - A - By Taha Sir/app.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
